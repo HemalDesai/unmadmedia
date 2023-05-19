a09ca8c6463fafdb45773d59073bd25f521b0cb6
HemalDesai <hemald332@gmail.com>
2023-05-19 21:45:15 +0530

    added stripe integration

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { loadStripe } from '@stripe/stripe-js';
+import { useState } from 'react';
+
+const stripePromise = loadStripe('pk_test_51N9UveSCOOyqFJgR7RagttAZlMeCk10wKTV5tNfKVTBopGA8C9YQUgKjUv0tOfHm9P4r0PzYZlRgv5r3kBFT0d3a00vC9E8msn');
 
 const Dashboard = () => {
+  const [loading, setLoading] = useState(false);
+  
+    const handleClick = async () => {
+      setLoading(true);
+      const stripe = await stripePromise;
+      console.log('Stripe:', stripe);
+      const { error } = await stripe.redirectToCheckout({
+        lineItems: [{ price: 'price_1N9UyNSCOOyqFJgRTG9dCmUL', quantity: 1 }],
+        mode: 'subscription',
+        successUrl: 'http://your-website.com/success',
+        cancelUrl: 'http://your-website.com/cancel',
+      });
+      if (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
   return (
     <div>
       <Navbar />
@@ -70,7 +91,9 @@ const Dashboard = () => {
           <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
             <div className="sub-content">
               
-              <button className="button">Upgrade </button>
+            <button onClick={handleClick} disabled={loading} className='button'>
+          {loading ? 'Loading...' : 'Upgrade'}
+        </button>
             </div>
           </div>
         </div>
